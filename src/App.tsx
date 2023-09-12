f91330637b3552cb45cb3ac5ad71f232f984660a
gonzalo <gonzalojpv@gmail.com>
2023-09-12 12:24:20 -0600

    GP, added logic and styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,52 @@ import "./App.css";
 import DefaultLayout from "./screens/layouts/default";
 import AddIdeaForm from "./components/AddIdeaForm";
 import IdeaList from "./components/IdeaList";
+import seed from "./utils/seed.json";
+
+import type { Idea } from "./types/ideas";
+
+import { useState } from "react";
 
 function App() {
+  const { ideas } = seed
+  const [items, setItems] = useState(ideas);
+
+  const useHandleAddItem = (newItem: Idea) => {
+    setItems([...items, newItem])
+  }
+
+  const handleUpIdea = (item: Idea) => {
+    const ideas = items.map((idea) => {
+      if (idea.id === item.id) {
+        idea.votes = idea.votes + 1
+      }      
+
+      return idea
+    })
+
+    setItems(ideas)
+  }
+
+  const handleDownIdea = (item: Idea) => {
+     const ideas = items.map((idea) => {
+      if (idea.id === item.id) {
+        if (idea.votes > 0) {
+          idea.votes = idea.votes - 1
+        }
+      }      
+
+      return idea
+    })
+
+    setItems(ideas)
+  }
+
   return (
     <>
       <DefaultLayout>
         <h1 className="mb-5 text-4xl text-center">IdeaBox</h1>
-        <AddIdeaForm />
-        <IdeaList />
+        <AddIdeaForm addIdea={useHandleAddItem} />
+        <IdeaList downIdea={handleDownIdea} upIdea={handleUpIdea} items={items} />
       </DefaultLayout>
     </>
   );
--- src/components/IdeaList/index.tsx
+++ src/components/IdeaList/index.tsx
@@ -1,28 +1,33 @@
 import arrowIcon from "/arrow.svg";
+import type { Idea, IdeaListProps } from "../../types/ideas";
 
-const ideaList = () => {
+const ideaList = ({ items, upIdea, downIdea }: IdeaListProps) => {
+
   return (
     <>
-      <article className="p-3 bg-gray-300 rounded-lg sm:flex sm:items-center">
+    {items.map((item: Idea) => 
+      <article key={item.name} className="p-3 mb-4 bg-gray-300 rounded-lg sm:flex sm:items-center idea">
         <section className="text-center sm:flex-1 sm:text-left">
           <h2 className="text-xl sm:text-2xl sm:leading-6">
-            Lorem ipsum dolor sit amet consectetur adipiscing elit, urna consequat felis vehicula
-            class ultricies mollis dictumst, aenean non a in donec nulla.
+          {item.name}
           </h2>
-          <small>UserName</small>
+          <small>{item.userName}</small>
         </section>
         <section className="pt-3 mt-6 border-t-2 border-black sm:pt-0 sm:pl-3 sm:border-t-0 sm:border-l-2 sm:flex sm:items-center">
-          <h3 className="text-3xl font-bold text-center">99</h3>
+          <h3 className="text-3xl font-bold text-center">{item.votes}</h3>
           <nav className="flex justify-center sm:block">
-            <img src={arrowIcon} alt="Vote up" className="w-10 cursor-pointer" />
+            <img src={arrowIcon} alt="Vote up" className="w-10 cursor-pointer" onClick={() => upIdea(item)} />
             <img
               src={arrowIcon}
               alt="Vote down"
               className="w-10 cursor-pointer transform rotate-180"
+              onClick={() => downIdea(item)}
             />
           </nav>
         </section>
       </article>
+    )}
+      
     </>
   );
 };
