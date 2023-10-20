d30ad9a119eb9bfdccb079d4d7c0119a5a333d3f
gonzalo <gonzalojpv@gmail.com>
2023-10-20 11:52:28 -0600

    GP, type to children: Context

--- src/contexts/auth-context.tsx
+++ src/contexts/auth-context.tsx
@@ -1,35 +1,33 @@
-import { createContext, useState, PropsWithChildren } from "react";
+import { createContext, useState, ReactNode } from "react";
 
 interface AuthContextProps {
-  isLoggedIn: boolean
-  onLogin: () => void
-  onLogout: () => void
+  isLoggedIn: boolean;
+  onLogin: () => void;
+  onLogout: () => void;
 }
+
+type Props = {
+  children: ReactNode;
+};
 
 export const AuthContext = createContext<AuthContextProps | undefined>(undefined);
 
-
-export const AuthContextProvider = ({ children }: PropsWithChildren<{}>) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+export const AuthContextProvider = ({ children }: Props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const logoutHandler = () => {
-    setIsLoggedIn(false)
-  }
+    setIsLoggedIn(false);
+  };
 
   const loginHandler = () => {
-    setIsLoggedIn(true)
-  }
+    setIsLoggedIn(true);
+  };
 
   const authContext: AuthContextProps = {
     isLoggedIn,
     onLogin: loginHandler,
-    onLogout: logoutHandler
-  }
+    onLogout: logoutHandler,
+  };
 
-  return (
-    <AuthContext.Provider value={authContext}>
-    {children}
-    </AuthContext.Provider>
-  )
-}
-
+  return <AuthContext.Provider value={authContext}>{children}</AuthContext.Provider>;
+};
